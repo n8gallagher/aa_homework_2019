1e73da6909b2130c4e7999af65ac2815645449d8
nate <n8gallagher@gmail.com>
2020-01-17 08:50:16 -0800

    updating hw files

--- W5D5/active-record_skelly/movie_novice/queries.rb
+++ W5D5/active-record_skelly/movie_novice/queries.rb
@@ -23,12 +23,14 @@
 
 def find_angelina
   #find Angelina Jolie by name in the actors table
+  Actor.find_by_name('Angelina Jolie')
 
 end
 
 def top_titles
   # get movie titles from movies with scores greater than or equal to 9
   # hint: use 'select' and 'where'
+  
 
 end
 

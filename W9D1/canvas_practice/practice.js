39326db9c48bbc2be86490488e3700415f9c308d
nate <n8gallagher@gmail.com>
2020-02-08 16:19:40 -0800

    making progress on canvas hw week 9 day 1

--- W9D1/canvas_practice/practice.js
+++ W9D1/canvas_practice/practice.js
@@ -1,17 +1,15 @@
+
+
 document.addEventListener("DOMContentLoaded", function(){
-  const canvas = document.getElementById("mycanvas");
-  canvas.height = 500;
-  canvas.width = 500;
-  const ctx = canvas.getContext('2d');
-  ctx.fillStyle(`rgb(255,255,255)`);
-  ctx.fillRect(20, 20, 100, 125);
+  const canvasEl = document.getElementById("mycanvas");
+  canvasEl.height = 500;
+  canvasEl.width = 500;
+  const ctx = canvasEl.getContext("2d");
+  ctx.fillStyle = "red";
+  ctx.fillRect(20, 20, 200, 200);
+
+  console.log(canvas);
  
 });
 
-// const canvas = Document.getElementById("mycanvas");
-// canvas.height = 500;
-// canvas.width = 500;
-// const ctx = canvas.getContext('2d');
 
-// canvas.fillStyle(`rgb(255,255,255)`);
-// ctx.fillRect(20, 20, 100, 125);

d79337cbd2ae2d94d30d5e097a9b81e326b57d53
nate <n8gallagher@gmail.com>
2020-02-08 16:07:38 -0800

    struggling with canvas

--- W9D1/canvas_practice/practice.js
+++ W9D1/canvas_practice/practice.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", function(){
-
+  const canvas = document.getElementById("mycanvas");
+  canvas.height = 500;
+  canvas.width = 500;
+  const ctx = canvas.getContext('2d');
+  ctx.fillStyle(`rgb(255,255,255)`);
+  ctx.fillRect(20, 20, 100, 125);
+ 
 });
+
+// const canvas = Document.getElementById("mycanvas");
+// canvas.height = 500;
+// canvas.width = 500;
+// const ctx = canvas.getContext('2d');
+
+// canvas.fillStyle(`rgb(255,255,255)`);
+// ctx.fillRect(20, 20, 100, 125);
